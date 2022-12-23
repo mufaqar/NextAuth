bc87a18d3740a9fcf2fd829d7c67f7d3132c322c
Mufaqar Islam <mufaqar@gmail.com>
2022-12-23 19:46:07 +0500

    profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { getSession } from 'next-auth/react';
 
-export default Profile = () => {
+export default function Profile() {
   return (
     <section className="container mx-auto text-center">
       <h3 className="text-4xl font-bold">Profile Page</h3>
@@ -9,7 +9,7 @@ export default Profile = () => {
       <Link href={'/'}>Home Page</Link>
     </section>
   );
-};
+}
 
 export async function getServerSideProps({ req }) {
   const session = await getSession({ req });
