72b5012c34987ce4339df3132fd3546c659159f1
Mufaqar Islam <mufaqar@gmail.com>
2022-12-23 19:21:49 +0500

    next auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,48 +1,63 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
+import FacebookProvider from 'next-auth/providers/facebook';
+import TwitterProvider from 'next-auth/providers/twitter';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import connectMongo from '../../../database/conn'
-import Users from '../../../model/Schema'
+import connectMongo from '../../../database/conn';
+import Users from '../../../model/Schema';
 import { compare } from 'bcryptjs';
 
 export default NextAuth({
-    providers : [
-        // Google Provider
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        CredentialsProvider({
-            name : "Credentials",
-            async authorize(credentials, req){
-                connectMongo().catch(error => { error: "Connection Failed...!"})
+  providers: [
+    // Google Provider
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      version: '2.0', // opt-in to Twitter OAuth 2.0
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      async authorize(credentials, req) {
+        connectMongo().catch((error) => {
+          error: 'Connection Failed...!';
+        });
 
-                // check user existance
-                const result = await Users.findOne( { email : credentials.email})
-                if(!result){
-                    throw new Error("No user Found with Email Please Sign Up...!")
-                }
+        // check user existance
+        const result = await Users.findOne({ email: credentials.email });
+        if (!result) {
+          throw new Error('No user Found with Email Please Sign Up...!');
+        }
 
-                // compare()
-                const checkPassword = await compare(credentials.password, result.password);
-                
-                // incorrect password
-                if(!checkPassword || result.email !== credentials.email){
-                    throw new Error("Username or Password doesn't match");
-                }
+        // compare()
+        const checkPassword = await compare(
+          credentials.password,
+          result.password
+        );
 
-                return result;
+        // incorrect password
+        if (!checkPassword || result.email !== credentials.email) {
+          throw new Error("Username or Password doesn't match");
+        }
 
-            }
-        })
-    ],
-    secret: "XH6bp/TkLvnUkQiPDEZNyHc0CV+VV5RL/n+HdVHoHN0=",
-    session: {
-        strategy: 'jwt',
-    }
-})
+        return result;
+      },
+    }),
+  ],
+  secret: 'XH6bp/TkLvnUkQiPDEZNyHc0CV+VV5RL/n+HdVHoHN0=',
+  session: {
+    strategy: 'jwt',
+  },
+});
